2d05cdbe0c8a2bcc6ed179d627b0fbc260c6fb5e
A.K.M Mainul Hasan <12734023-mainulhasanbd@users.noreply.gitlab.com>
2023-02-05 23:53:59 +0600

    python learning day one

---
+++ Day_01/hello.py
@@ -0,0 +1,20 @@
+print('Hello World!')
+
+a = 10;
+b= 20;
+
+print(a+b)
+
+if a > b:
+    print('a is greater than b')
+else:
+    print('b is greater than a')
+
+    if (a+b == 30):
+      print('a+b is equal to 30')
+    else:
+      print('a+b is not equal to 30')
+
+# Path: Day_01\hello.py
+
+# End of Day 01
